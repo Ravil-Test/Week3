10a612e0162a5332cf66ebb80eacebdf44763e76
Titran <kolanho3@gmail.com>
2024-07-23 19:11:16 +0300

    Реализация таблицы "Поставщик" на JavaJdbc с файйлом конфигурации DB

--- src/main/java/ru/nurullin/config/SpringConfig.java
+++ src/main/java/ru/nurullin/config/SpringConfig.java
@@ -3,6 +3,8 @@ package ru.nurullin.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import ru.nurullin.DAO.SupplierDao;
@@ -11,10 +13,18 @@ import ru.nurullin.service.SupplierService;
 import ru.nurullin.service.SupplierServiceImp;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 @ComponentScan(basePackages = {"ru.nurullin.DAO", "ru.nurullin.service"})
+@PropertySource("classpath:database.properties")
 public class SpringConfig {
+
+    private final Environment environment;
+
+    public SpringConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public JdbcTemplate getJdbcTemplate() {
@@ -24,10 +34,10 @@ public class SpringConfig {
     @Bean
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("root");
-        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setUsername(environment.getProperty("username"));
+        dataSource.setPassword(environment.getProperty("password"));
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
         return dataSource;
     }
 
